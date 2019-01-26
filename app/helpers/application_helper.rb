48f58fbd7d35b5046bd662193939dbd0c8a8822c
KhIgorLe <igor.kharchuk85@gmail.com>
2019-01-26 13:28:02 +0200

    fixed flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def github_url(author, repo, options)
     link_to repo.camelize, "https://github.com/#{author}/#{repo}", options
   end
+
+  def flash_class(key)
+    case key
+      when 'notice' then 'alert alert-info'
+      when 'success' then 'alert alert-success'
+      when 'warning' then 'alert alert-warning'
+    end
+  end
 end
