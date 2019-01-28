c4a2813c5ee22bc1fb0a460d3bcb9e6ab6f53e0d
KhIgorLe <igor.kharchuk85@gmail.com>
2019-01-28 11:42:35 +0200

    API add git ignore .env

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,5 @@ module TestGuru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    config.autoload_paths << "#{Rails.root}/lib/clients"
   end
 end
